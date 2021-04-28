faeccba241d082144cdd4c221532ef7888d366bc
Mateusz Kaminski <mateusz@DESKTOP-30E2KQS.localdomain>
2021-04-28 15:34:28 +0200

    Dodanie nowej metody wyswietl Hello Gdansk

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 public class Main{
 
 public static void main(String[] args){
-
+    
 System.out.println("Hello World");
+}
+
+
+public void wyswietlHelloGdansk(){
+System.out.println("Hello Gdansk");
+
 }
 
 }
